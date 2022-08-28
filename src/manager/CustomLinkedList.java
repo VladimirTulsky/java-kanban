23afd4f9695ef1fd142695f50c066163101c37c6
Vladimir <vladimit.tulskiy@yandex.ru>
2022-08-28 20:17:22 +0300

    feat : custom List implementation

---
+++ src/manager/CustomLinkedList.java
@@ -0,0 +1,47 @@
+package manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomLinkedList<T> {
+    public Node<T> head;
+    public Node<T> tail;
+
+    public void addLast(T t) {
+        if (head == null) {
+            Node<T> currentNode = new Node<>(t, null, tail);
+            head = currentNode;
+            tail = new Node<>(null, currentNode, null);
+            return;
+        }
+        Node<T> currentNode = tail;
+        currentNode.value = t;
+        tail = new Node<>(null, currentNode, null);
+        currentNode.prev.next = currentNode;
+        currentNode.next = tail;
+    }
+
+    public void removeNode(Node<T> node) {
+        if (node.equals(head)) {
+            head = node.next;
+            if (node.next != null) {
+                node.next.prev = null;
+            }
+        } else {
+            node.prev.next = node.next;
+            if (node.next != null) {
+                node.next.prev = node.prev;
+            }
+        }
+    }
+
+    public List getTasks() {
+        List<T> historyList = new ArrayList<>();
+        Node<T> item = head;
+        while(item.value != null) {
+            historyList.add(item.value);
+            item = item.next;
+        }
+        return historyList;
+    }
+}
---
+++ src/manager/Node.java
@@ -0,0 +1,13 @@
+package manager;
+
+class Node<T> {
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
+
+    public Node(T value, Node<T> prev, Node<T> next) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+}
